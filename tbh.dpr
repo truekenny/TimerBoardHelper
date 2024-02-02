f896bfc5a7eb120d557a65e706cdaadca2678c51
Truekenny <sasha@yaroslavtsev.net>
2024-02-02 23:05:46 +0300

    init

---
+++ tbh.dpr
@@ -0,0 +1,14 @@
+﻿program tbh;
+
+uses
+  Vcl.Forms,
+  UnitMain in 'UnitMain.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
